3706ce85e00a9a0e74454984e85bb24692e7afc2
Nikolay <nikolka-net@mail.ru>
2019-11-16 22:00:57 +0600

    Добавил условие вывода нулей, если дедлайн закончился; подставил 0 перед значениями, состоящими из одной цифры

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+window.addEventListener('DOMContentLoaded', function () {//загрузка только DOM, без картинок
+  'use strict';
+  //Timer
+  function countTimer(deadline) {
+    let timerDays = document.querySelector('#timer-days'),
+      timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
+
+    function getTimeRemaining() {
+      function addZero(num) {//добавляем нули
+        let str = num.toString();
+        return str.length == 1 ? "0" + str : str;
+      }
+
+      let dateStop = new Date(deadline).getTime(),//дата окончания
+        dateNow = new Date().getTime(),//текущая дата, миллисекунды
+        timeRemaining = (dateStop - dateNow) / 1000,//количество оставшихся секунд
+        seconds = addZero(Math.floor(timeRemaining % 60)),//получаем секунды
+        minutes = addZero(Math.floor((timeRemaining / 60) % 60)),//получаем минуты
+        hours = addZero(Math.floor(timeRemaining / 60 / 60) % 24),//получаем часы
+        days = addZero(Math.floor(timeRemaining / 60 / 60 / 24));//получаем дни
+
+      return {//возвращение в виде объекта
+        timeRemaining, days, hours, minutes, seconds
+      };
+    }
+
+    function updateClock() {
+      let timer = getTimeRemaining();
+
+      timerDays.textContent = timer.days;
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+
+      if (timer.timeRemaining > 0) {
+        setTimeout(updateClock, 1000);
+      } else {
+        timerDays.textContent = '00';
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+      }
+    }
+    updateClock();
+
+  }
+
+  countTimer('01 february 2020');
+  // setInterval(countTimer, 1000, '01 february 2020');//обновление через 1000мс
+  // console.log('countTimer: ', countTimer);
+
+});
