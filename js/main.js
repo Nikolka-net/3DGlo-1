020133ad0d6a0d0401e6532c66d962faa7b9ed91
Nikolay <nikolka-net@mail.ru>
2019-11-18 20:43:08 +0600

    Написан скрипт меню и модального окна

--- js/main.js
+++ js/main.js
@@ -41,14 +41,56 @@ window.addEventListener('DOMContentLoaded', () => {//загрузка тольк
         timerHours.textContent = '00';
         timerMinutes.textContent = '00';
         timerSeconds.textContent = '00';
-        clearInterval(idInterval);
+        //clearInterval(idInterval);
       }
     };
     updateClock();
 
   };
 
-  //countTimer('17 november 2019 15:32');
-  let idInterval = setInterval(countTimer, 1000, '1 january 2020');//обновление через 1000мс
+  countTimer('1 january 2020');
+  //let idInterval = setInterval(countTimer, 1000, '1 january 2020');//обновление через 1000мс
+
+  //Menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');//с помощью css
+      //альтернативный код
+      /*  if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {//если transform есть, оно превр. в false и отработ. else
+         menu.style.transform = `translate(0)`;
+       } else {
+         menu.style.transform = `translate(-100%)`;
+       } */
+    };
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));//сворачивание меню
+  };
+
+  toggleMenu();
+
+  //popup
+
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popUpClose = document.querySelector('.popup-close');
+
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popup.style.display = 'block';//вызываем модальное окно
+      });
+    });
+    popUpClose.addEventListener('click', () => {
+      popup.style.display = 'none';//закрываем модальное окно
+    });
+
+  };
+  togglePopUp();
 
 });
