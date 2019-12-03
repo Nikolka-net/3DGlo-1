5eac660f26e6e0020d402adcb04fcc1bc2ff3e19
Nikolay <nikolka-net@mail.ru>
2019-12-03 21:31:35 +0600

    Подключил полифиллы, собрал проект с по-ю webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+import "@babel/polyfill";//решает многие проблемы совместимости
+import 'nodelist-foreach-polyfill';//подключ. полифил foreach для IE11
+import elementClosest from 'element-closest';
+elementClosest(window);//вызываем его
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+
 import countTimer from './modules/countTimer';
 import toggleMenu from './modules/toggleMenu';
 import togglePopUp from './modules/togglePopUp';
